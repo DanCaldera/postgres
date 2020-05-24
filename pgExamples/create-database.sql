c498047b55a6ae9c8401cdea8623382797745865
Daniel Caldera <bdcaldera@gmail.com>
2020-05-23 23:23:57 -0500

    initial commit

---
+++ pgExamples/create-database.sql
@@ -0,0 +1,16 @@
+-- Database: course_data
+
+-- DROP DATABASE course_data;
+
+CREATE DATABASE course_data
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+	
+	
+	
+	
